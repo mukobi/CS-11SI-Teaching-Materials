d3bb592a1d614d142695b2e8485a067f369dcb52
Gabriel Mukobi <gabrielmukobi@gmail.com>
2022-06-01 22:02:42 -0700

    Some more class demos

--- Intro to Unity/Assets/Demos/Lab 3 - More Scripting/PitOfDoom.cs
+++ Intro to Unity/Assets/Demos/Lab 3 - More Scripting/PitOfDoom.cs
@@ -1,10 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadingManager : MonoBehaviour
+{
+    public void LoadSceneByName(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void GoBackToTitleScene()
+    {
+        LoadSceneByName("Title");
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
+
+public class GameManager : MonoBehaviour
+{
+    public bool hasPlayerCompletedTheFirstTask = false;
+    public GameObject referenceToThePlayer;
+    public int numberOfFlowerPotsLeftToWater = 5;
+
+    public static GameManager Instance = null;
+    private void Awake()
+    {
+        // You might consider keeping the GameManager from getting destroyed
+        // when you change scenes and preventing multiple from existing.
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(this);
+        // Other scripts can now access me with GameManager.Instance instead
+        // of having to find the game manager on Start
+    }
+}
+
 
 public class PitOfDoom : MonoBehaviour
 {
     public Transform RespawnPosition;
+
+    private GameManager gameManager;
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        gameManager.DoStuff();
+    }
+
 
     void OnTriggerEnter(Collider other)
     {
